d59c47f11097a92bc36aa2748ffd1951f87636d7
Andrews Carlos <andrews.knct@gmail.com>
2022-12-07 09:36:51 -0300

    feat: add sub (#1)
    * feat: add sub
    
    * fix: sum test
    
    * fix: sub test
    
    * feat: adjust sonar properties

--- main_test.go
+++ main_test.go
@@ -3,8 +3,15 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	result := sum(2, 2)
+	result := sum(2, 3)
 	if result != 5 {
 		t.Error("The result must be 5")
+	}
+}
+
+func TestSub(t *testing.T) {
+	result := sub(2, 2)
+	if result != 0 {
+		t.Error("The result must be 0")
 	}
 }
